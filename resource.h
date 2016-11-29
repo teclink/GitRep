2dd71a8d7a0182c29646f32075ee08adc5a80308
tec_moyz <374401281@qq.com>
2016-11-29 13:52:43 +0800

    5files

---
+++ resource.h
@@ -0,0 +1,97 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by √≈µÍœµÕ≥.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MYTYPE                      129
+#define IDD_CBZX                        130
+#define IDD_HYSELE                      131
+#define IDD_HYDA                        132
+#define IDD_XSMXB                       133
+#define IDB_BackGround                  136
+#define IDI_SETICON                     138
+#define IDI_CHECKICON                   139
+#define IDI_ESCICON                     140
+#define IDI_HELPICON                    141
+#define IDI_OUTICON                     142
+#define IDI_PRINTICON                   143
+#define IDI_REFRESHICON                 144
+#define IDD_XSMXBCX                     146
+#define IDD_XSHZB                       147
+#define IDD_XSHZBCX                     148
+#define IDD_XSTH                        149
+#define IDC_EDIT2                       1001
+#define IDC_EDIT3                       1002
+#define IDC_EDIT4                       1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT5                       1005
+#define IDC_EDIT6                       1006
+#define IDC_EDIT7                       1007
+#define IDC_EDIT8                       1008
+#define IDC_EDIT10                      1011
+#define IDC_DATALIST                    1015
+#define IDC_SETBUTTON                   1016
+#define IDC_PRINTBUTTON                 1017
+#define IDC_BUTTON1                     1017
+#define IDC_BUTTON2                     1018
+#define IDC_OUTBUTTON                   1020
+#define IDC_CHECKBUTTON                 1021
+#define IDC_REFRESHBUTTON               1022
+#define IDC_HELPBUTTON                  1023
+#define IDC_ESCBUTTON                   1024
+#define IDC_SalesreturnBUTTON           1025
+#define ID_MENUITEM32771                32771
+#define ID_MENUITEM32772                32772
+#define ID_MENUITEM32773                32773
+#define ID_MENUITEM32774                32774
+#define ID_MENUITEM32775                32775
+#define ID_MENUITEM32776                32776
+#define ID_MENUITEM32777                32777
+#define ID_MENUITEM32778                32778
+#define ID_MENUITEM32779                32779
+#define ID_MENUITEM32780                32780
+#define ID_MENUITEM32781                32781
+#define ID_MENUITEM32782                32782
+#define ID_MENUITEM32783                32783
+#define ID_MENUITEM32784                32784
+#define ID_MENUITEM32785                32785
+#define ID_MENUITEM32786                32786
+#define ID_MENUITEM32787                32787
+#define ID_MENUITEM32788                32788
+#define ID_MENUITEM32789                32789
+#define ID_MENUITEM32790                32790
+#define ID_MENUITEM32791                32791
+#define ID_MENUITEM32792                32792
+#define ID_MENUITEM32793                32793
+#define ID_MENUITEM32794                32794
+#define ID_MENUITEM32795                32795
+#define ID_MENUITEM32796                32796
+#define ID_MENUITEM32797                32797
+#define ID_MENUITEM32798                32798
+#define ID_MENUITEM32799                32799
+#define ID_MENUITEM32800                32800
+#define ID_MENUITEM32801                32801
+#define ID_MENUITEM32802                32802
+#define ID_MENUITEM32803                32803
+#define ID_MENUITEM32804                32804
+#define ID_MENUITEM32805                32805
+#define ID_MENUITEM32806                32806
+#define ID_MENUITEM32807                32807
+#define ID_MENUITEM32808                32808
+#define ID_MENUITEM32809                32809
+#define ID_MENUITEM32810                32810
+#define ID_MENUITEM32811                32811
+#define ID_MENUITEM32812                32812
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        150
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
